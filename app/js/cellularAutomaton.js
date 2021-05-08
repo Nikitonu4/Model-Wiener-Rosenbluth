93c69a3aca3ac28e17e61c18f3f8e4b8a433a060
Nikitonu4 <nikitonu4@gmail.com>
2021-05-08 05:53:27 +0300

    80% complete

--- app/js/cellularAutomaton.js
+++ app/js/cellularAutomaton.js
@@ -1,49 +1,148 @@
+"use strict;";
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-plusplus */
 /* eslint-disable class-methods-use-this */
+
+// ПОКОЙ, ВОЗБУЖДЕНИЕ, РЕФРАКТЕРНОСТЬ
+// Рефрактерность - элемент не может возбуждаться, через
+// const s = 13; // через s состояний элемент возвращается в состояние покоя
+// const r = 6; // Рефрактерность после r состояний(до r - элемент возбужден)
+// const h = 5; // пороговое значение концентрации активатора, после него элемент возбуждается
+
+// через s шагов после возбуждения элемент возвращается в состояние покоя.
+// из s в состояние покоя => концентрация активатора u[i][j] = 0
+// s>r !!!!!!!!!!!!
+
+// есть возбужденный сосед => концентрация элемента u[i][j]++
+// Если l ближайших соседей возбуждены, то на соот
+// ветствующем шаге к предыдущему значению концентрации активатора
+// прибавляется число возбужденных соседей
+
 class CellularAutomaton {
-  constructor(N = 110, M = 90, s = 13, r = 6, h = 5, cellSize = 6) {
-    this.N = N;
-    this.M = M;
+  constructor(s = 13, r = 6, h = 5, cellSize = 10) {
     this.s = s;
     this.r = r;
     this.h = h;
     this.y = [];
     this.yy = [];
     this.u = [];
-    this.k = 60;
-    this.cells = []; // массив клеток
-    this.buffCells = [];
+    this.cellSize = cellSize;
+    this.t = 0;
 
     this.cellSize = cellSize; // размер клетки
     this.grid = document.getElementById("grid").getContext("2d"); // канвас для сетки
     this.canvas = document.getElementById("canvas").getContext("2d");
 
-    this.numberHeightCells = 1100; // количество клеток по высоте
-    this.numberWidthCells = 900; // количество клеток по ширине
+    this.numberHeightCells = 100; // количество клеток по высоте
+    this.numberWidthCells = 100; // количество клеток по ширине
+
+    this.setSizeCanvases();
     this.init();
     this.createNullArrays();
 
-    this.t = 0;
+    // this.oneWave();
+    // this.wavesBelousovZhabotinsky();
+    this.singleSleeveWave();
+    // this.doubleSleeveWave();
+    this.main();
+  }
 
-    for (let i = 0; i < this.size.x; i++) {
-      // реакция Белоусова-Жаботинского
-      for (let j = 0; j < this.size.y; j++) {
-        this.y[i][j] = Math.round(this.getRandomArbitrary(1, 400) / 10);
-        if (this.y[i][j] > 15) this.y[i][j] = 0;
-      }
+  setSizeCanvases() {
+    // изменяет размер канваса в зависимости от cellSize
+    /** Установили количество ячеек */
+    const width = this.div(
+      document.querySelector(".cells-wrapper").offsetWidth,
+      this.cellSize
+    );
+    this.numberWidthCells = width;
+
+    const height = this.div(
+      document.querySelector(".cells-wrapper").offsetHeight,
+      this.cellSize
+    );
+    this.numberHeightCells = height;
+
+    /** установили новый размер канвасов */
+    this.grid.canvas.height = height * this.cellSize;
+    this.grid.canvas.width = width * this.cellSize;
+
+    this.canvas.canvas.height = height * this.cellSize;
+    this.canvas.canvas.width = width * this.cellSize;
+  }
+
+  init() {
+    this.grid.clearRect(0, 0, this.grid.width, this.grid.height);
+    this.grid.width = this.cellSize * this.numberWidthCells;
+    this.grid.height = this.cellSize * this.numberHeightCells;
+
+    this.size = { x: this.numberWidthCells, y: this.numberHeightCells };
+    this.gridDraw();
+  }
+
+  gridDraw() {
+    this.grid.beginPath();
+    const widthGrid = this.numberWidthCells * this.cellSize;
+    const heightGrid = this.numberHeightCells * this.cellSize;
+
+    /*
+    задаю параметры сетки
+    */
+    this.grid.strokeStyle = "#000";
+    this.grid.lineWidth = 1;
+    // рисуем сетку
+    for (let i = 0; i <= this.size.y; i++) {
+      this.grid.moveTo(0, i * this.cellSize + 0.5);
+      this.grid.lineTo(widthGrid, i * this.cellSize + 0.5);
     }
 
-    // for (let j = 0; j < this.numberWidthCells; j++) {
-    //   for (let i = 0; i < 40; i++) {
-    //     this.y[40 + i][j] = i;
-    //     if (j > 40) {
-    //       this.y[40 + i][j] = 14 - i;
-    //     }
-    //   }
-    // }
+    for (let i = 0; i <= this.size.x; i++) {
+      this.grid.moveTo(i * this.cellSize + 0.5, 0);
+      this.grid.lineTo(i * this.cellSize + 0.5, heightGrid);
+    }
 
-    this.main();
-    // this.y[50][50] = 1;
-    // this.fillCell(50, 50);
+    this.grid.stroke();
+  }
+
+  fillGrid() {
+    this.clearCanvas();
+    for (let i = 0; i < this.size.x; i++) {
+      for (let j = 0; j < this.size.y; j++) {
+        this.y[i][j] = this.yy[i][j];
+        let color = "Yellow";
+        // let color = colors[this.y[i][j]];
+        if (this.y[i][j] >= 1 && this.y[i][j] <= this.r) {
+          color = "Gray";
+          // color = colors[this.y[i][j]];
+          this.fillCell(i, j, color);
+        }
+        if (this.y[i][j] > 6 && this.y[i][j] <= this.s) {
+          this.fillCell(i, j, color);
+        }
+
+        if (this.y[i][j] == 0) {
+          this.fillCell(i, j, "White");
+        }
+      }
+    }
+  }
+
+  clearCanvas() {
+    this.canvas.clearRect(0, 0, this.grid.width, this.grid.height);
+  }
+
+  fillCell(x, y, color = "Black") {
+    this.canvas.fillStyle = color;
+    this.canvas.fillRect(
+      x * this.cellSize,
+      y * this.cellSize,
+      this.cellSize,
+      this.cellSize
+    );
+  }
+
+  div(a, b) {
+    return (a - (a % b)) / b;
   }
 
   getRandomArbitrary(min, max) {
@@ -64,10 +163,11 @@ class CellularAutomaton {
   }
 
   step() {
-    console.log(this.t);
     this.t++;
-    for (let i = 2; i < this.size.x; i++) {
-      for (let j = 2; j < this.size.y; j++) {
+    // this.osc1(this.t);
+    // this.osc2(this.t);
+    for (let i = 1; i < this.size.x; i++) {
+      for (let j = 1; j < this.size.y; j++) {
         if (this.y[i][j] > 0 && this.y[i][j] < this.s) {
           this.yy[i][j] = this.y[i][j] + 1;
         }
@@ -92,11 +192,7 @@ class CellularAutomaton {
       }
     }
 
-    // for (let i = 0; i < this.size.x; i++) {
-    //   for (let j = 0; j < this.size.y; j++) {
-    //     this.y[i][j] = this.yy[i][j];
-    //   }
-    // }
+    // this.obstacle();
 
     this.fillGrid();
   }
@@ -105,138 +201,64 @@ class CellularAutomaton {
     this.step();
     setTimeout(() => {
       this.main();
-    }, 20);
-    // while (t != 1000) {
-
-    // дальше вывод
-    // for (let i = 0; i <= this.N; i++) {
-    //   for (let j = 0; j <= this.M; j++) {
-    //     this.y[i][j] = this.yy[i][j];
-    //     if (this.y[i][j] >= 1 && this.y[i][j] <= this.r) {
-    //       this.fillCell(i, j);
-    //     }
-    //     if (this.y[i][j] > 6 && this.y[i][j] <= this.s) {
-    //       this.fillCell(i, j);
-    //     }
-    //   }
-    // }
-    // }
+    }, 5);
   }
 
-  init() {
-    this.grid.clearRect(0, 0, this.grid.width, this.grid.height);
-    /** устанавливаем размеры */
-    this.grid.width = this.cellSize * this.numberWidthCells; // умножаем на numberWidthCells.value
-    this.grid.height = this.cellSize * this.numberHeightCells;
-
-    this.size = { x: this.numberWidthCells, y: this.numberHeightCells };
-    this.newGridFill();
-    this.gridDraw();
-  }
-
-  newGridFill() {
-    // создание новой сетки
+  // реакция Белоусова-Жаботинского
+  wavesBelousovZhabotinsky() {
     for (let i = 0; i < this.size.x; i++) {
-      this.cells[i] = [];
-      this.buffCells[i] = [];
       for (let j = 0; j < this.size.y; j++) {
-        this.cells[i][j] = false;
-        this.buffCells[i][j] = false;
+        this.y[i][j] = Math.round(this.getRandomArbitrary(1, 400) / 10);
+        if (this.y[i][j] > 15) this.y[i][j] = 0;
       }
     }
   }
 
-  gridDraw() {
-    this.grid.beginPath();
-    const widthGrid = this.numberWidthCells * this.cellSize;
-    const heightGrid = this.numberHeightCells * this.cellSize;
-
-    /*
-    задаю параметры сетки
-    */
-    this.grid.strokeStyle = "rgb(50, 50, 50)";
-    this.grid.lineWidth = 1;
-
-    // рисуем сетку
-    for (let i = 0; i <= this.size.y; i++) {
-      this.grid.moveTo(0, i * this.cellSize + 0.5);
-      this.grid.lineTo(widthGrid, i * this.cellSize + 0.5);
-    }
-
-    for (let i = 0; i <= this.size.x; i++) {
-      this.grid.moveTo(i * this.cellSize + 0.5, 0);
-      this.grid.lineTo(i * this.cellSize + 0.5, heightGrid);
-    }
-
-    this.grid.stroke();
-
-    // делаем фон
-    // this.grid.beginPath();
-    // this.grid.fillStyle = "#213b32";
-    // this.grid.fillRect(this.numberWidthCells * this.cellSize, 0, 10000, 10000);
-    // this.grid.fillRect(0, this.numberHeightCells * this.cellSize, 10000, 10000);
-
-    // this.grid.stroke();
+  // одиночная волна
+  oneWave() {
+    this.y[Math.round(this.size.x / 2)][Math.round(this.size.y / 2)] = 1;
   }
 
-  // randomFill() {
-  //   /** Заполняем матрицу */
-  //   for (let i = 0; i < this.size.x; i++) {
-  //     for (let j = 0; j < this.size.y; j++) {
-  //       const fill = Math.random() < 0.5;
-  //       this.cells[i][j] = fill;
-  //     }
-  //   }
-  //   /** Отрисовываем */
-  //   this.fillGrid();
-  // }
+  // однорукавная волна
+  singleSleeveWave() {
+    for (let i = 0; i < this.size.x / 2; i++) {
+      for (let j = 0; j < this.s; j++) {
+        this.y[Math.round(this.size.x / 2) + j][i] = j;
+      }
+    }
+  }
 
-  fillGrid() {
-    const colors = [
-      "Black",
-      "Blue",
-      "Green",
-      "Cyan",
-      "Red",
-      "#FF00FF",
-      "Brown",
-      "LightGray",
-      "DarkGray",
-      "LightBlue",
-      "LightGreen",
-      "LightCyan",
-      "LightRed",
-      "LightMagnenta",
-      "Yellow",
-      "White",
-    ];
-    this.clearCanvas();
-    for (let i = 0; i < this.size.x; i++) {
-      for (let j = 0; j < this.size.y; j++) {
-        this.y[i][j] = this.yy[i][j];
-        let color = "Pink";
-        if (this.y[i][j] >= 1 && this.y[i][j] <= this.r) {
-          color = "Gray";
-          this.fillCell(i, j, color);
-        }
-        if (this.y[i][j] > 6 && this.y[i][j] <= this.s) {
-          this.fillCell(i, j, color);
+  // двурукавная волна
+  doubleSleeveWave() {
+    for (let j = 0; j < this.numberWidthCells; j++) {
+      for (let i = 0; i < this.s; i++) {
+        this.y[40 + i][j] = i;
+        if (j > 40) {
+          this.y[40 + i][j] = 14 - i;
         }
       }
     }
   }
 
-  clearCanvas() {
-    this.canvas.clearRect(0, 0, this.grid.width, this.grid.height);
+  // первый осциллятор
+  osc1(t) {
+    if (t == Math.round(t / 20) * 20) {
+      this.y[30][30] = 1;
+    }
   }
 
-  fillCell(x, y, color = "black") {
-    this.canvas.fillStyle = color;
-    this.canvas.fillRect(
-      x * this.cellSize,
-      y * this.cellSize,
-      this.cellSize,
-      this.cellSize
-    );
+  // второй осциллятор
+  osc2(t) {
+    if (t == Math.round(t / 30) * 30) {
+      this.y[20][50] = 1;
+    }
+  }
+
+  // препятствие
+  obstacle() {
+    for (let i = 0; i < 70; i++) {
+      this.yy[i][60] = 0;
+      this.yy[i][61] = 0;
+    }
   }
 }
--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,75 @@
 "use strict;";
-// ПОКОЙ, ВОЗБУЖДЕНИЕ, РЕФРАКТЕРНОСТЬ
-// Элемент в покое, если y[i][j] == 0
-// Рефрактерность - элемент не может возбуждаться, через
-// const N = 50;
-// const M = 50;
-// const s = 13; // через s состояний элемент возвращается в состояние покоя
-// const r = 6; // Рефрактерность после r состояний(до r - элемент возбужден)
-// const h = 5; // пороговое значение концентрации активатора, после него элемент возбуждается
 
-// через s шагов после возбуждения элемент возвращается в состояние покоя.
-// из s в состояние покоя => концентрация активатора u[i][j] = 0
-// s>r !!!!!!!!!!!!
+// обработка перемещения панели управления
+dragElement(document.querySelector(".control__panel"));
 
-// есть возбужденный сосед => концентрация элемента u[i][j]++
-// Если l ближайших соседей возбуждены, то на соот- ветствующем шаге к предыдущему значению концентрации активатора
-// прибавляется число возбужденных соседей
+function dragElement(el) {
+  let pos1 = 0;
+  let pos2 = 0;
+  let pos3 = 0;
+  let pos4 = 0;
+  if (document.querySelector(`.${el.className}-header`)) {
+    // если присутствует, заголовок - это место, откуда вы перемещаете DIV:
+    document.querySelector(
+      `.${el.className}-header`
+    ).onmousedown = dragMouseDown;
+  } else {
+    // в противном случае переместите DIV из любого места внутри DIV:
+    el.onmousedown = dragMouseDown;
+  }
 
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // получить положение курсора мыши при запуске:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // вызов функции при каждом перемещении курсора:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // вычислить новую позицию курсора:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // установите новое положение элемента:
+    el.style.top = `${el.offsetTop - pos2}px`;
+    el.style.left = `${el.offsetLeft - pos1}px`;
+  }
+
+  function closeDragElement() {
+    // остановка перемещения при отпускании кнопки мыши:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+// s шагов до покоя
+// r состояний возбуждения
+// h пороговая концентрация активатора
+// размер клеток
+// Количество клеток по вертикали
+// Количество клеток по горизонтали
+// Интервал шага(в мс)
+// Шаг
+// запуск/стоп
+// сбросить все
+
+// RadioButtonы
+/*
+1) реакция Белоусова-Жаботинского
+2) одиночная волна
+3) однорукавная волна
+4) двурукавная волна
+
+
+
+5) 1 осциллятор
+6) 2 осциллятор
+7) препятствие - checkbox
+*/
 new CellularAutomaton();
